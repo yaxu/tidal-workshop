4dd011c6279ddd9070408e0babbf7d2d8f39aa81
alex <alex@slab.org>
2018-03-09 13:49:11 +0000

    / ‘limerick/compile.sh’

--- limerick/compile.sh
+++ limerick/compile.sh
@@ -4,4 +4,4 @@ if [ -d "$HOME/.cabal/bin" ] ; then
     PATH="$HOME/.cabal/bin:$PATH"
 fi
 
-pandoc -s 0*.md --template=templates/template.latex --from markdown+auto_identifiers -V colorlinks --number-sections --pdf-engine=xelatex --variable mainfont="Linux Biolinum O"  --filter render.hs -o tidal.pdf
+pandoc -s 0*.md --from markdown+auto_identifiers -V colorlinks --number-sections --pdf-engine=xelatex --variable mainfont="Linux Biolinum O"  --filter render.hs -o tidal.pdf
