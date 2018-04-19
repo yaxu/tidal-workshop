ca262ab5534bbb1e90fc98ac3abb44b4ef2fec9f
alex <alex@slab.org>
2018-04-19 09:38:05 +0200

    + ‘karlsruhe/01.log’ + ‘karlsruhe/01.md’ + ‘karlsruhe/compile.sh’ + ‘karlsruhe/limerick.tidal’ + ‘karlsruhe/render.hs’ + ‘karlsruhe/runpattern.hs’ + ‘karlsruhe/tidal.pdf’

---
+++ karlsruhe/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d "$HOME/.cabal/bin" ] ; then
+    PATH="$HOME/.cabal/bin:$PATH"
+fi
+
+pandoc -s 0*.md --from markdown+auto_identifiers -V colorlinks --number-sections --filter render.hs -o tidal.pdf
