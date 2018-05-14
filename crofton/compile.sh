480572db076864a89b93d5a69790e5a4407ff1f3
alex <alex@slab.org>
2018-05-14 23:39:22 +0100

    - ‘crofton/.compile.sh.swp’ / ‘crofton/compile.sh’

--- crofton/compile.sh
+++ crofton/compile.sh
@@ -4,4 +4,4 @@ if [ -d "$HOME/.cabal/bin" ] ; then
     PATH="$HOME/.cabal/bin:$PATH"
 fi
 
-pandoc -s 0*.md --from markdown+auto_identifiers -V colorlinks --number-sections --filter render.hs --latex-engine=xelatex -o tidal.pdf
+pandoc -s 0*.md --from markdown+auto_identifiers -V colorlinks --number-sections --filter render.hs --pdf-engine=xelatex -o tidal.pdf
